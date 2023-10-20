d1498973b7ca780c2ec368c616f66b699be2cd92
NathanPREVOST <nathanprvst@gmail.com>
2023-10-20 20:41:24 +0200

    Script1980

--- Scrap_Training/Practice.py
+++ Scrap_Training/Practice.py
@@ -8,7 +8,7 @@ import time
  # Parser la page
 #Product = page.findAll("div","flex flex-1 flex-col p-3") 
 # Permet d'avoir tout les produits ( comics )
-test2 = ''
+
 for compteur in range(1,56,1) :
     site = 'https://www.lacentrale.fr/listing?makesModelsCommercialNames=&options=&page='+str(compteur)+'&yearMax=1980'
     req = Request(site,headers={'User-agent':'Mozilla/5.0'})
@@ -20,8 +20,17 @@ for compteur in range(1,56,1) :
     for CompteurVoiture in range(1,16,1):
         voiture = page.findAll("a",class_="Vehiculecard_Vehiculecard_vehiculeCard Containers_Containers_containers Containers_Containers_borderRadius Containers_Containers_darkShadowWide")
         nom = voiture[CompteurVoiture].findAll("h2",class_="Text_Text_text Vehiculecard_Vehiculecard_title Text_Text_subtitle2")
+        prix = voiture[CompteurVoiture].findAll("span",class_="Text_Text_text Vehiculecard_Vehiculecard_price Text_Text_subtitle2")
+        AKBC = voiture[CompteurVoiture].findAll("div",class_="Vehiculecard_Vehiculecard_characteristics")
+        ModelPrecis = voiture[CompteurVoiture].findAll("div",class_="Text_Text_text Vehiculecard_Vehiculecard_subTitle Text_Text_body2")
+
+
+        print('\n')
         print(nom[0].text)
+        print(ModelPrecis[0].text)
+        print(AKBC[0].text+'')
+        print(prix[0].text)
 
+        
 
-print (nom)
 
